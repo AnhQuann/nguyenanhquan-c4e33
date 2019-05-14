01f97073d7b011f8e11296d4ff4bad7d80d9081b
AnhQuann <anhquan19051905@gmail.com>
2019-05-14 22:01:02 +0700

    lan dau lam chuyen ay

---
+++ session1/intro.js
@@ -0,0 +1,24 @@
+// let name = 1;
+// name = 5;
+// console.log('Hello', name);
+
+// Đây là code cộng 2 số
+// let x = Number(prompt("Nhập số thứ nhất"));
+// let y = Number(prompt("Nhập số thứ hai"));
+// console.log(x + y);
+
+clear()
+
+for (let i = 0; i < 10; i++) {
+  fd(100)
+  rt(90)
+  fd(100)
+  rt(90)
+  fd(100)
+  rt(90)
+  fd(100)
+  rt(90)
+  
+  rt(30)
+} 
+
